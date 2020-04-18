5a38f358e23859e19e29ada144e8a9a5e66d3910
Hamish <leithhamish@gmail.com>
2020-04-18 21:26:38 +0100

    Updates

---
+++ precache-manifest.c8988aa6f07261c6aa2150a00b981c31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cef3ec52dd731832373e773025947ffa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abc0743ae659f0c4e3e4",
+    "url": "/static/css/main.9ca87392.chunk.css"
+  },
+  {
+    "revision": "d6824cf00b3fa7b82d6e",
+    "url": "/static/js/2.0e62950d.chunk.js"
+  },
+  {
+    "revision": "d00098666f3a9b89d824bdeb15ece8eb",
+    "url": "/static/js/2.0e62950d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abc0743ae659f0c4e3e4",
+    "url": "/static/js/main.7e22ef1a.chunk.js"
+  },
+  {
+    "revision": "4604617d8d106ffb2817",
+    "url": "/static/js/runtime-main.d32be798.js"
+  }
+]);
